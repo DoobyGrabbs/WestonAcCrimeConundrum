11fd413c2779d165efa740ed0b9de46968b07682
Dave Groves <microsoft@groverhome.co.uk>
2022-01-27 18:20:20 +0000

    Add project files.

---
+++ WestonAcCrimeConundrum/Services/CrimeRestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace WestonAcCrimeConundrum.Services
+{
+    public class CrimeRestService
+    {
+        public CrimeRestService()
+        {
+
+        }
+
+        public string GetCrimeDataByLatLngDate(string lat, string lng, DateOnly date)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // httpClient.DefaultRequestHeaders.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
+                var response = httpClient.GetStringAsync(new Uri("https://data.police.uk/api/crimes-street/all-crime")).Result;
+                return response;
+            }
+        }
+    }
+}
